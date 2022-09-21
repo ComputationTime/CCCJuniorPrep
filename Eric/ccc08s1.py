801599dba4e128b9e6ed5d82df0eeeba1fa44fa6
eric11055 <ec2020canada@gmail.com>
2022-09-20 20:40:38 -0700

    ccc08s1

---
+++ Eric/ccc08s1.py
@@ -0,0 +1,21 @@
+input_finish = False
+
+lowest_temp = 201
+lowest_temp_city = ''
+
+
+while input_finish == False:
+    curr_line = input().split()
+    curr_line[1] = int(curr_line[1])
+    if curr_line[0].strip() == 'Waterloo':
+        input_finish = True
+    if curr_line[1] < lowest_temp:
+        lowest_temp = curr_line[1]
+        lowest_temp_city = curr_line[0]
+
+print(lowest_temp_city)
+
+    
+
+    
+    
