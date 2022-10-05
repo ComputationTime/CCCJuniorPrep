91c0b43f53bccf33f73ee4a61b41e8e28386e106
Andre <mellowcoding@gmail.com>
2022-10-04 19:25:42 -0700

    class 5

---
+++ Andre/Class4J3.py
@@ -0,0 +1,25 @@
+# N = int(input())
+
+# def cool_print(n, *messages):
+#     for i in range(n):
+#         for message in messages:
+#             print(n*message, end='')
+#         print()
+
+# cool_print(N, '*', 'x', '*')
+# cool_print(N, ' ', 'x', 'x')
+# cool_print(N, '*', ' ', '*')
+
+
+array = ['a', 'b', 'c', 'd']
+
+for i in range(len(array)):
+    array[i]
+    pass
+
+for element in array:
+    pass
+
+for i, element in enumerate(array):
+    print(i)
+    print(element)
