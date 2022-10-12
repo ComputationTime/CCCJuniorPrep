6691ea392c4d111113f00393ff492737b6e9aba7
eric11055 <ec2020canada@gmail.com>
2022-10-11 20:19:20 -0700

    10/11

---
+++ Eric/ccc19j3.py
@@ -0,0 +1,34 @@
+#really scuffed but it works on dmoj
+
+
+N = int(input())
+
+linelist = []
+
+
+def check(linelist):
+    output = ""
+    counter = 0
+    curr_letter = ""
+    for i in linelist:
+        counter += 1
+        if curr_letter == "":
+            curr_letter = i
+            counter -= 1
+        if curr_letter != i:
+            output += (str(counter) + " " +str(curr_letter) + " ")
+            curr_letter = i
+            counter = 0
+
+    output += (str(counter+1) + " " +str(curr_letter))
+        
+    return output
+
+    
+
+
+
+
+for i in range(N):
+    line = input()
+    print(check(line))
