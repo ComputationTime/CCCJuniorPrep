ebf7149fd0c03ead323098756f1381b3895732bf
eric11055 <ec2020canada@gmail.com>
2022-11-15 19:03:56 -0800

    11/15

--- Eric/ccc10j5.py
+++ Eric/ccc10j5.py
@@ -1,10 +1,81 @@
-Ay, Ax = input()
-By, Bx = input()
-steps = 0
+Ax, Ay = input().split()
+Bx, By = input().split()
+Ax = int(Ax)
+Ay = int(Ay)
+Bx = int(Bx)
+By = int(By)
 
-def search(knighty,knightx,targety,targetx):
-    steps 
-    if knighty == targety and knightx == targetx:
-        pass
+completion = False
+
+searchlist = []
+
+explored_tiles = []
+
+def search(knightx,knighty,steps):
+    global output
+    global completion
+    explored_tiles.append([knightx,knighty])
+    if completion != True:
+        if knightx == Bx and knighty == By:
+            completion = True
+            output = steps
+        else: 
+            searchlist.append(knightx-1)
+            searchlist.append(knighty-2)
+            searchlist.append(steps+1)
+
+            searchlist.append(knightx+1)
+            searchlist.append(knighty-2)
+            searchlist.append(steps+1)
+
+            searchlist.append(knightx-1)
+            searchlist.append(knighty+2)
+            searchlist.append(steps+1)
+
+            searchlist.append(knightx+1) 
+            searchlist.append(knighty+2)
+            searchlist.append(steps+1)
+
+            searchlist.append(knightx-2)
+            searchlist.append(knighty-1)
+            searchlist.append(steps+1)
+
+            searchlist.append(knightx+2)
+            searchlist.append(knighty-2)
+            searchlist.append(steps+1)
+
+            searchlist.append(knightx-2)
+            searchlist.append(knighty+1)
+            searchlist.append(steps+1)
+
+            searchlist.append(knightx+2)
+            searchlist.append(knighty+1)
+            searchlist.append(steps+1)
+                    
+
+search(Ax,Ay,0)
+
+while completion != True:
+    x = searchlist.pop(0)
+    y = searchlist.pop(1)
+    steps = searchlist.pop(2)
+    search(x,y,steps)
+
+print(output)
+                      
+                    
+
+
+
+
+
+
+
+
+
+
+    
+
+        
 
 
