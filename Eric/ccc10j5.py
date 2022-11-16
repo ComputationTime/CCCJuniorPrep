a8642cf097fedd44889b350ec1a345e64aaaa87d
eric11055 <ec2020canada@gmail.com>
2022-11-15 20:34:59 -0800

    11/15

--- Eric/ccc10j5.py
+++ Eric/ccc10j5.py
@@ -14,52 +14,75 @@ explored_tiles = []
 def search(x,y,steps):
     global output
     global completion
-    explored_tiles.append(x,y)
+    explored_tiles.append(f"{x} {y}")
     if x == Bx and y == By:
         completion = True
         output = steps
     else:
-
         # only append stuff that hasn't been explored yet. AKA use your explored_tiles list (this is for efficiency only)
-        searchlist.append(x-1)
-        searchlist.append(y-2)
-        searchlist.append(steps+1)
+        if f"{x-1} {y-2}" in explored_tiles:
+            pass
+        else:
+            searchlist.append(x-1)
+            searchlist.append(y-2)
+            searchlist.append(steps+1)
 
-        searchlist.append(x+1)
-        searchlist.append(y-2)
-        searchlist.append(steps+1)
+        if f"{x+1} {y-2}" in explored_tiles:
+            pass
+        else:
+            searchlist.append(x+1)
+            searchlist.append(y-2)
+            searchlist.append(steps+1)
 
-        searchlist.append(x-1)
-        searchlist.append(y+2)
-        searchlist.append(steps+1)
+        if f"{x-1} {y+2}" in explored_tiles:
+            pass
+        else:
+            searchlist.append(x-1)
+            searchlist.append(y+2)
+            searchlist.append(steps+1)
 
-        searchlist.append(x+1) 
-        searchlist.append(y+2)
-        searchlist.append(steps+1)
+        if f"{x+1} {y+2}" in explored_tiles:
+            pass
+        else:
+            searchlist.append(x+1) 
+            searchlist.append(y+2)
+            searchlist.append(steps+1)
 
-        searchlist.append(x-2)
-        searchlist.append(y-1)
-        searchlist.append(steps+1)
+        if f"{x-2} {y-1}" in explored_tiles:
+            pass
+        else:
+            searchlist.append(x-2)
+            searchlist.append(y-1)
+            searchlist.append(steps+1)
 
-        searchlist.append(x+2)
-        searchlist.append(y-2)
-        searchlist.append(steps+1)
+        if f"{x+2} {y-1}" in explored_tiles:
+            pass
+        else:
+            searchlist.append(x+2)
+            searchlist.append(y-1)
+            searchlist.append(steps+1)
 
-        searchlist.append(x-2)
-        searchlist.append(y+1)
-        searchlist.append(steps+1)
-
-        searchlist.append(x+2)
-        searchlist.append(y+1)
-        searchlist.append(steps+1)
+        if f"{x-2} {y+1}" in explored_tiles:
+            pass
+        else:
+            searchlist.append(x-2)
+            searchlist.append(y+1)
+            searchlist.append(steps+1)
+            
+        if f"{x+2} {y+1}" in explored_tiles:
+            pass
+        else:
+            searchlist.append(x+2)
+            searchlist.append(y+1)
+            searchlist.append(steps+1)
                 
 
 search(Ax,Ay,0)
 
 while completion != True:
     x = searchlist.pop(0) # pop also removes x from the list
-    y = searchlist.pop(1) # this means you should pop(0) again instead of pop(1)
-    steps = searchlist.pop(2) # and this should also be pop(0)
+    y = searchlist.pop(0) # this means you should pop(0) again instead of pop(1)
+    steps = searchlist.pop(0) # and this should also be pop(0)
     if x < 10 and x > -1:
         if y < 10 and y > -1:
             search(x,y,steps)
