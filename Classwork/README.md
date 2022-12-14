b2ab5e04c3b6fdd5b05e09c0faad679053bc86c0
Andre <mellowcoding@gmail.com>
2022-12-13 19:01:01 -0800

    new class material

--- Classwork/README.md
+++ Classwork/README.md
@@ -3,97 +3,173 @@
 ## Class 3
 
 ### J1
+
 https://dmoj.ca/problem/ccc08s1
+
 ### J2
+
 https://dmoj.ca/problem/ccc17j2
 
 ## Class 4
 
 ### J1
+
 https://dmoj.ca/problem/ccc01j1
+
 ### J2
+
 https://dmoj.ca/problem/ccc18j2
+
 ### J3
+
 https://dmoj.ca/problem/ccc12j3
+
 ### J4
+
 https://dmoj.ca/problem/ccc20j4
+
 ### J5
+
 https://dmoj.ca/problem/ccc11j5
 
 ## Class 5
 
 ### J1
+
 https://dmoj.ca/problem/ccc04j1
+
 ### J2
+
 https://dmoj.ca/problem/ccc19j2
+
 ### J3
+
 https://dmoj.ca/problem/hkccc15j3
+
 ### J4
+
 https://dmoj.ca/problem/ccc06j4
+
 ### J5
+
 https://dmoj.ca/problem/ccc04j5
 
-## Class 6 
+## Class 6
 
 ### J1
+
 https://dmoj.ca/problem/ccc03j1
+
 ### J2
+
 https://dmoj.ca/problem/ccc13j2
+
 ### J3
+
 https://dmoj.ca/problem/ccc19j3
+
 ### J4
+
 https://dmoj.ca/problem/ccc08j4
+
 ### J5
+
 https://dmoj.ca/problem/ccc06j5
 
 ## Class 7
 
 ### J1
-https://dmoj.ca/problem/ccc05j1
-### J2
-https://dmoj.ca/problem/ccc14j2
-### J3
-https://dmoj.ca/problem/ccc16j3
 
+https://dmoj.ca/problem/ccc05j1
+
+### J2
+
+https://dmoj.ca/problem/ccc14j2
+
+### J3
+
+https://dmoj.ca/problem/ccc16j3
 
 ## Class 9
 
 ### J4
+
 https://dmoj.ca/problem/ccc08j4
+
 ### J4
+
 https://dmoj.ca/problem/ccc13j4
+
 ### J5
+
 https://dmoj.ca/problem/ccc10j5
 
 ## Class 11
 
 ### J1
+
 https://dmoj.ca/problem/ccc16j1
+
 ### J2
+
 https://dmoj.ca/problem/ccc15j2
+
 ### J3
+
 https://dmoj.ca/problem/ccc20j3
+
 ### J4
+
 https://dmoj.ca/problem/ccc07j4
 
 ## Class 12
 
 ### J1
+
 https://dmoj.ca/problem/ccc12j1
+
 ### J2
+
 https://dmoj.ca/problem/ccc01j2
+
 ### J2
+
 https://dmoj.ca/problem/ccc04j2
+
 ### J5
+
 https://dmoj.ca/problem/ccc04j5
 
-## Class 13 -- TODAY
+## Class 13
 
 ### 1
+
 https://dmoj.ca/problem/ccc00j1
+
 ### 2
+
 https://dmoj.ca/problem/ccc00j2
-### 3
+
+## Class 14
+
+### 1
+
 https://dmoj.ca/problem/ccc02j1
-### 4
+
+### 2
+
 https://dmoj.ca/problem/ccc02j2
+
+## Class 15 -- Today
+
+### 1
+
+https://dmoj.ca/problem/ccc03j2
+
+### 2
+
+https://dmoj.ca/problem/ccc04j1
+
+### 3
+
+https://dmoj.ca/problem/ccc05j4
--- Eric/ccc04j5.py
+++ Eric/ccc04j5.py
@@ -1,75 +1,81 @@
-# import matplotlib.pyplot as plt
-level,width,x = input().split()
-level = int(level)
-width = int(width)
-x = int(x)
+def eric(level, width, x):
 
 
-# x_list = []
-# y_list = []
+    # x_list = []
+    # y_list = []
 
-point_list = [(0,1),(width,1)]
-
-
-def level_up(Ax,Ay,Bx,By):
-    x_change = Bx - Ax
-    y_change = By - Ay
-    if x_change != 0:
-        l = x_change // 3
-        point_list.append((Ax,Ay))
-        point_list.append((Ax + l,Ay))
-        point_list.append((Ax + l,Ay + l))
-        point_list.append((Ax + (l * 2),Ay + l))
-        point_list.append((Ax + (l * 2),Ay))
-        point_list.append((Ax + (l * 3),Ay))
-    else:
-        l = y_change // 3
-        point_list.append((Ax,Ay))
-        point_list.append((Ax,Ay+l))
-        point_list.append((Ax - l,Ay + l))
-        point_list.append((Ax - l,Ay + (l * 2)))
-        point_list.append((Ax,Ay  + (l * 2)))
-        point_list.append((Ax,Ay  + (l * 3)))
+    point_list = [(0,1),(width,1)]
 
 
-for i in range(level):
-    point_count = len(point_list)
-    for i in range(len(point_list)-1):
+    def level_up(Ax,Ay,Bx,By):
+        x_change = Bx - Ax
+        y_change = By - Ay
+        if x_change != 0:
+            l = x_change // 3
+            point_list.append((Ax,Ay))
+            point_list.append((Ax + l,Ay))
+            point_list.append((Ax + l,Ay + l))
+            point_list.append((Ax + (l * 2),Ay + l))
+            point_list.append((Ax + (l * 2),Ay))
+            point_list.append((Ax + (l * 3),Ay))
+        else:
+            l = y_change // 3
+            point_list.append((Ax,Ay))
+            point_list.append((Ax,Ay+l))
+            point_list.append((Ax - l,Ay + l))
+            point_list.append((Ax - l,Ay + (l * 2)))
+            point_list.append((Ax,Ay  + (l * 2)))
+            point_list.append((Ax,Ay  + (l * 3)))
+
+
+    for i in range(level):
+        point_count = len(point_list)
+        for i in range(len(point_list)-1):
+            point1 = point_list[i]
+            Ax = point1[0]
+            Ay = point1[1]
+            point2 = point_list[i+1]
+            Bx = point2[0]
+            By = point2[1]
+            level_up(Ax,Ay,Bx,By)
+        for _ in range(point_count):
+            point_list.pop(0)
+
+    backup = point_list
+    point_list = []
+    for point in backup:
+        if point not in point_list:
+            point_list.append(point)
+
+    # import matplotlib.pyplot as plt
+    # fx = lambda tup : tup[0]
+    # fy = lambda tup : tup[1]
+    # x = list(map(fx, point_list))
+    # y = list(map(fy, point_list))
+    # print(point_list)
+    # plt.plot(x, y)
+    # plt.show()
+
+        
+
+    output_list = []
+
+
+
+    for i in range(0,len(point_list)-1):
         point1 = point_list[i]
-        Ax = point1[0]
-        Ay = point1[1]
         point2 = point_list[i+1]
-        Bx = point2[0]
-        By = point2[1]
-        level_up(Ax,Ay,Bx,By)
-    for _ in range(point_count):
-        point_list.pop(0)
+        # I like this solution to figure out the intersection much better. Way cleaner than mine
+        # make sure to remember range stops one point before the second number
+        if x in range(point1[0],point2[0] + 1) or x in range(point2[0],point1[0] + 1):
+            if point1[1] not in output_list:
+                output_list.append(point1[1])
 
-# for i in point_list:
-#     x_list.append(i[0])
-#     y_list.append(i[1])
+    output_list = sorted(output_list)
 
-    
+    output = ' '.join(map(str,output_list))
 
-output_list = []
-
-
-
-for i in range(0,len(point_list)-1):
-    point1 = point_list[i]
-    point2 = point_list[i+1]
-    if x in range(point1[0],point2[0]) or x in range(point2[0],point1[0]):
-        output_list.append(point1[1])
-
-for point in point_list:
-    if point[0] == x and point[1] not in output_list:
-        output_list.append(point[1])
-
-output_list = sorted(output_list)
-
-output = ' '.join(map(str,output_list))
-
-print(output)
+    return output
 
 # plt.plot(x_list, y_list, '-o')
 # plt.show()
