a29db035a195ff50f024592096430412b9a29e29
eric11055 <ec2020canada@gmail.com>
2022-12-20 20:42:53 -0800

    12/20

---
+++ Eric/ccc07j2.py
@@ -0,0 +1,25 @@
+dict = {
+    'CU':'see you',
+    ':-)':"I'm happy",
+    ':-(':"I'm unhappy",
+    ';-)':'wink',
+    ':-P':'stick out my tongue',
+    '(~.~)':'sleepy',
+    'TA':'totally awesome',
+    'CCC':'Canadian Computing Competition',
+    'CUZ':'because',
+    'TY':'thank-you',
+    'YW':"you're welcome",
+    'TTYL':'talk to you later'
+}
+
+
+while True:
+    word = input()
+    if word in dict:
+        print(dict[word])
+    else:
+        print(word)
+    if word == "TTYL":
+        break
+    
